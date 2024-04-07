71ce04ac395b1b1a5819451328fb1a0ff8048cf6
KafayatYusuf <155323901+KafayatYusuf@users.noreply.github.com>
2024-04-07 13:15:54 +0100

    Imported library to enable add security features

--- API/auth/__init__.py
+++ API/auth/__init__.py
@@ -2,26 +2,21 @@ from enum import Enum
 from typing import Union
 
 from fastapi import Depends, Header, HTTPException
+from fastapi.security import APIKeyHeader
 from osm_login_python.core import Auth
 from pydantic import BaseModel, Field
 
 from src.app import Users
 from src.config import get_oauth_credentials
 
-API_access_token = APIKeyHeader(name="access_token", description="user_authentication")
-
 
 class UserRole(Enum):
-    """Assigning user role as integer"""
-
     ADMIN = 1
     STAFF = 2
     GUEST = 3
 
 
 class AuthUser(BaseModel):
-    """Defining fields as attributes"""
-
     id: int
     username: str
     img_url: Union[str, None]
@@ -32,14 +27,12 @@ osm_auth = Auth(*get_oauth_credentials())
 
 
 def get_user_from_db(osm_id: int):
-    """Get OSM ID from the user"""
     auth = Users()
     user = auth.read_user(osm_id)
     return user
 
 
 def get_osm_auth_user(access_token):
-    """Get OSM access token from the user"""
     try:
         user = AuthUser(**osm_auth.deserialize_access_token(access_token))
     except Exception as ex:
@@ -51,15 +44,11 @@ def get_osm_auth_user(access_token):
     return user
 
 
-def login_required(access_token: str = Depends(API_access_token)):
-    """Get user's login details"""
-    if access_token != "my_token":
-        raise HTTPException(status_code=401, detail="Invalid API Key")
+def login_required(access_token: str = Header(...)):
     return get_osm_auth_user(access_token)
 
 
 def get_optional_user(access_token: str = Header(default=None)) -> AuthUser:
-    """Get user's access token which is optional"""
     if access_token:
         return get_osm_auth_user(access_token)
     else:
@@ -68,7 +57,6 @@ def get_optional_user(access_token: str = Header(default=None)) -> AuthUser:
 
 
 def admin_required(user: AuthUser = Depends(login_required)):
-    """Get admin login details"""
     db_user = get_user_from_db(user.id)
     if not db_user["role"] is UserRole.ADMIN.value:
         raise HTTPException(status_code=403, detail="User is not an admin")
@@ -76,7 +64,6 @@ def admin_required(user: AuthUser = Depends(login_required)):
 
 
 def staff_required(user: AuthUser = Depends(login_required)):
-    """Get staff login details"""
     db_user = get_user_from_db(user.id)
 
     # admin is staff too
